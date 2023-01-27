b66e5538ec99ba3c5cf9e0c326b2890c0b04144b
ArterioRodrigues <arterio369@gmail.com>
2023-01-26 23:10:38 -0500

    week one finish

--- week_1/Program_1/main.py
+++ week_1/Program_1/main.py
@@ -1,7 +1,7 @@
 """
     Name: Arterio Rodrigues
     Email: arterio.rodrigues47@myhunter.cuny.edu
-    Resources:  python3.8 documents 
+    Resources:  python3.8 documents
 """
 
 def make_dictionary(data, kind = "min"):
@@ -18,49 +18,32 @@ def make_dictionary(data, kind = "min"):
 
     #Placeholder-- replace with your code
     new_dict = {}
-    if(kind == "station"):
+    if kind == "station":
         for line in data:
             line = line.strip()
             line = line.split(',')
 
             new_dict[line[1]+','+line[2]] = line[3]
-        return new_dict
 
-    data_dict = {}
-
-    for line in data:
-        line = line.strip()
-        line = line.split(',')
-        
-        data_dict[line[1]+','+line[2]] = []
-        new_dict[line[1]+','+line[2]] = 0
-
-    for line in data: 
-        line = line.strip()
-        line = line.split(',')
-
-        data_dict[line[1]+','+line[2]].append(line[9])
-
-    for element in data_dict:
-        entries_arr = data_dict[element]
-
-        if(kind == "min"):
-            value = int(entries_arr[0])
-        else:
-            value = 0
-        #print(element, entries_arr[:5] , entries_arr[-5:], end ='')
-
-        for i in range(1, len(entries_arr)): 
-            if(kind == "min"):
-                if( value > (int(entries_arr[i]) - int(entries_arr[i-1]))):
-                    value = int(entries_arr[i]) - int(entries_arr[i-1])
+    else:
+        for line in data:
+            line = line.strip()
+            line = line.split(',')
+            if kind == "max":
+                new_dict[line[1]+','+line[2]] = 0
             else:
-                 if( value < (int(entries_arr[i]) - int(entries_arr[i-1]))):
-                    value = int(entries_arr[i]) - int(entries_arr[i-1])
-        new_dict[element] = value
-        #print(" *****", value)
-   
-       
+                new_dict[line[1]+','+line[2]] = int(line[9])
+
+        for line in data:
+            line = line.strip()
+            line = line.split(',')
+
+            if kind == "max":
+                if new_dict[line[1]+','+line[2]] < int(line[9]):
+                    new_dict[line[1]+','+line[2]] = int(line[9])
+            else:
+                if new_dict[line[1]+','+line[2]] > int(line[9]):
+                    new_dict[line[1]+','+line[2]] = int(line[9])
 
     return new_dict
 
@@ -72,12 +55,12 @@ def get_turnstiles(station_dict, stations = None):
     Returns a list.
 
     Keyword arguments:
-    stations -- None or list of station names.   
+    stations -- None or list of station names.
     """
 
     #Placeholder-- replace with your code
     lst = []
-    if(stations == None):
+    if stations is None:
         for element in station_dict:
             lst.append(element)
     else:
@@ -96,16 +79,18 @@ def compute_ridership(min_dict,max_dict,turnstiles = None):
     across all turnstiles specified.
 
     Keyword arguments:
-    turnstiles -- None or list of turnstile names    
+    turnstiles -- None or list of turnstile names
     """
 
     #Placeholder-- replace with your code
     total = 0
-    if(turnstiles == None):
-        for i in range(len(min_dict)):
+
+    if turnstiles is None:
+        for i in min_dict:
             total += max_dict[i] - min_dict[i]
+
     else:
-        for turnstile in turnstiles: 
+        for turnstile in turnstiles:
             total += max_dict[turnstile]-min_dict[turnstile]
 
     return total
@@ -125,7 +110,7 @@ def main():
     min_dict = make_dictionary(data, kind = "min")
     max_dict = make_dictionary(data, kind = "max")
     station_dict = make_dictionary(data, kind = "station")
-    
+
     #Print out the station names, alphabetically, without duplicates:
     print(f'All stations: {sorted(list(set(station_dict.values())))}')
 
@@ -133,7 +118,7 @@ def main():
     print(f'All turnstiles: {get_turnstiles(station_dict)}')
     #Only those for Hunter & Roosevelt Island stations:
     print(get_turnstiles(station_dict, stations = ['68ST-HUNTER CO','ROOSEVELT ISLND']))
-    
+
     #Checking the ridership for a single turnstile
     ridership = compute_ridership(min_dict,max_dict,turnstiles=["R051,02-00-00"])
     print(f'Ridership for turnstile, R051,02-00-00:  {ridership}.')
@@ -145,6 +130,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-#194015   R527,R122,00-05-00,90 ST-ELMHURST,7,IRT,06/04/2022,00:00:00,REGULAR,1188687309,1639063620 
-#194056   R527,R122,00-05-00,90 ST-ELMHURST,7,IRT,06/10/2022,20:00:00,REGULAR,1188679966,1639060777  
